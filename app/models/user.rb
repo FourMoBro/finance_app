212b55d2b12777988426dfe7bbee377fdceccb20
Randall Lisy <lisyrj@gmail.com>
2016-03-25 17:23:28 +0000

    add search friends ui

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
   has_many :friendships
   has_many :friends, through: :friendships
   
+  
+  
   def full_name
     return "#{first_name} #{last_name}".strip if (first_name || last_name)
     "Anonymous"
